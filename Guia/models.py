1e6bd3b0f85b40bda7d93b68d3b62fadc7a2ded2
Kevin Yasuda <kevinshingo@hotmail.com>
2019-04-29 22:40:46 +0200

    Se agrego Guia

--- Guia/models.py
+++ Guia/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Guia(models.Model):
     nombre = models.CharField(max_length=100) #Campo/columna titulo de tipo "campo de caracteres" de longitud maxima de 100
-    sexo = models.IntegerField(max_length=100)
+    sexo = models.CharField(max_length=100)
     foto = models.ImageField(max_length=100)
     idioma = models.CharField(max_length=100)
     biografia = models.TextField(null=True, blank=True) #Campo/columna titulo de tipo Texto, los argumentos blank y null son para que el campo sea opcional
--- turismo/settings.py
+++ turismo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Guia',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-py'
 
-TIME_ZONE = 'GMT-4'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
