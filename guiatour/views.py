a746d09f2b12831067600ca11c6e05e1a71fcb04
mathiasbaezz <mathiasbaezz@gmail.com>
2019-04-30 22:18:49 +0200

    Se verifico algunas cosas

--- guiatour/views.py
+++ guiatour/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Ciudad
+from .models import Guia
 # Create your views here.
+
+
+def salida(request):
+    despedida = Ciudad.objects.all()
+    despe = Guia.objects.all()
+    #return HttpResponse(despedida)
+    return render(request,"index.html",{"ciudades":despedida, "guias":despe })
--- turismo/urls.py
+++ turismo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from guiatour import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ciudad/', views.salida),
 ]
