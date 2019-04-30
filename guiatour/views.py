3b9183420044376de7c6bb2533ea08c534f8326f
Kevin Yasuda <kevinshingo@hotmail.com>
2019-04-30 15:50:06 +0200

    Hicimos view/home

--- guiatour/views.py
+++ guiatour/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from guiatour.models import Ciudad
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    id_asuncion = Ciudad.objects.all()[0].id
+    id_encarnacion = Ciudad.objects.all()[1].id
+    id_cde = Ciudad.objects.all()[2].id
+    contexto = {
+        "id_asuncion" : id_asuncion,
+        "id_encarnacion" : id_encarnacion, 
+        "id_cde" : id_cde,
+    }
+
+    return HttpResponse(id_encarnacion)
