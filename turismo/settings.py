2bb50cf79c1038f60f1257d5f10a3c9a03900a1e
WilliBobadilla <Willi1997.1@gmail.com>
2019-06-23 11:11:26 -0400

    cambiado el setting de debug

--- turismo/settings.py
+++ turismo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dxmd*51i12alwox!)iv+lkuj^71rxc0kkbn)n9t)mev%!e6&5b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']   #permite el despliegue en todos los hosts
 
